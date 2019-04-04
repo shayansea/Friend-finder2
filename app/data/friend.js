4c3b91a2f98c6d5e7c8aeee0bdabc6b84d7b815d
Shayan Anoushiravani <shayananoushiravani@Shayans-MacBook-Pro.local>
2019-04-04 09:35:15 -0700

    added survey

--- app/data/friend.js
+++ app/data/friend.js
@@ -4,13 +4,13 @@ var friendsArray = [
 		name: 'Slytherin',
 		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
 		scores: [
-			'1',
 			'5',
 			'5',
-			'1',
 			'5',
 			'5',
-			'1',
+			'3',
+			'5',
+			'5',
 			'1',
 			'1',
 			'5'
@@ -20,16 +20,16 @@ var friendsArray = [
 		name: 'Gryffindor',
 		photo: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
 		scores: [
-			'2',
-			'4',
 			'1',
-			'1',
-			'1',
-			'1',
+			'3',
 			'1',
 			'2',
+			'5',
 			'1',
-			'5'
+			'2',
+			'2',
+			'5',
+			'3'
 		]
 		},
 		{
@@ -37,30 +37,30 @@ var friendsArray = [
 			photo: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
 			scores: [
 				'2',
+				'3',
+				'1',
+				'1',
+				'5',
 				'4',
 				'1',
-				'1',
-				'1',
-				'1',
-				'1',
-				'2',
-				'1',
-				'5'
+				'5',
+				'5',
+				'3'
 			]
 			},
 			{
 				name: 'Ravenclaw',
 				photo: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
 				scores: [
+					'5',
+					'4',
+					'4',
+					'5',
 					'2',
 					'4',
+					'5',
 					'1',
-					'1',
-					'1',
-					'1',
-					'1',
-					'2',
-					'1',
+					'3',
 					'5'
 				]
 				},
